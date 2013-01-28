8f59b8048eeaff612423a356b4521fa30a534891
Jonas Degrave <Jonas.Degrave@UGent.be>
2013-01-28 23:32:46 +0100

    Committing merge part 3

--- srv.py
+++ srv.py
@@ -6,10 +6,6 @@ import getopinionated.local_settings
 status = os.system('python manage.py validate')
 
 if(not status):
-    try:
-        os.remove(DATABASE_NAME)
-    except:
-        pass
     os.system('python manage.py syncdb --noinput')
     os.system('python manage.py loaddata testdata.json')
     os.system('python manage.py runserver 8000')
