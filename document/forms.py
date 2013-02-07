b522a97902b43a3f945e99c6f0a871795ef6f805
Jonas Degrave <Jonas.Degrave@UGent.be>
2013-02-07 02:16:21 +0100

    Pre-fill in the proposal entry form with the text in the document

--- document/forms.py
+++ document/forms.py
@@ -2,13 +2,17 @@ from django import forms
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from models import Diff
+from document.models import FullDocument
 
-class ProposalForm(forms.Form):
-    proposal = forms.CharField(label="",widget=forms.Textarea(attrs={'cols': 80, 'rows': 20}))
+class ProposalForm(forms.ModelForm):
+    content = forms.CharField(label="",widget=forms.Textarea(attrs={'cols': 80, 'rows': 20}))
 
-    def __init__(self, instance, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(ProposalForm, self).__init__(*args, **kwargs)
-        self.fulldocument = instance
+        
+    class Meta:
+        model = FullDocument
+        fields = ("content",)
 
     def save(self, commit=True):
         pass
