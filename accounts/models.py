f821659b838071091c8d2e8d4ee3ccd38e8bbe69
Jonas Degrave <Jonas.Degrave@UGent.be>
2013-02-16 05:57:48 +0100

    added karma

--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from common.stringify import niceBigInteger
 
 class UserProfile(models.Model):  
     user = models.OneToOneField(User, related_name="profile")
-
+    karma = models.IntegerField(default=0)
+    
     @property
     def display_name(self):
         return self.user.username
-
+    
 ### make sure profile is created when new user is created ###
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
