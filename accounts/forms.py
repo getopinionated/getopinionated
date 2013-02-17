167bfc7957ef7730aa1c7e57a08ecc2626daabe8
Jens Nyman <nymanjens.nj@gmail.com>
2013-02-17 16:43:26 +0100

    added slug to profile and fixed some unique problems with it (slug has to be unique, but is not necessary so when only checking for unique username)

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,19 @@
+import re
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.forms import AuthenticationForm
-from common.stringify import int_to_roman
+from django.template.defaultfilters import slugify
 
-import re
+from common.stringify import int_to_roman
+from models import UserProfile
+
+error_messages = {
+    'duplicate_username': _("A user with that username already exists."),
+    'duplicate_email': _("A user with that email address already exists."),
+    'password_mismatch': _("The two password fields didn't match."),
+}
 
 class ProfileCreationForm(UserCreationForm):
     
@@ -22,25 +30,16 @@ class ProfileCreationForm(UserCreationForm):
             'invalid': _("This value may contain only letters, numbers and "
                          "@/./+/-/_ characters.")})
     
-    error_messages = {
-        'duplicate_username': _("A user with that username already exists."),
-        'duplicate_email': _("A user with that email address already exists."),
-        'password_mismatch': _("The two password fields didn't match."),
-    }
-
     def clean_username(self):
-        # Since User.username is unique, this check is redundant,
-        # but it sets a nicer error message than the ORM. See #13147.
+        # check if slug is unique (via UserProfile.isValidUserName)
         username = self.cleaned_data["username"]
+        if not UserProfile.isValidUserName(username):
+            raise forms.ValidationError(error_messages['duplicate_username'])
         try:
-            User.objects.get(username=username)
+            User.objects.get(email=username)
         except User.DoesNotExist:
-            try:
-                User.objects.get(email=username)
-            except User.DoesNotExist:
-                return username
-            raise forms.ValidationError(self.error_messages['duplicate_email'])
-        raise forms.ValidationError(self.error_messages['duplicate_username'])
+            return username
+        raise forms.ValidationError(error_messages['duplicate_email'])
 
     def save(self, commit=True):
         user = super(ProfileCreationForm, self).save(commit=False)
@@ -51,24 +50,16 @@ class ProfileCreationForm(UserCreationForm):
             user.email = user.username
             #extract the username from the mailaddress
             username = user.username.split("@", 1)[0] #take the part before the @ in the mail address as username
-            username = username.replace('_',' ')
-            username = username.replace('.',' ')
+            username = username.replace('.','_')
             #if this username would already exist, add roman numbers until this is not the case
-            try:
-                User.objects.get(username=username)
-                #if we get here, the username does already exist
+            if not UserProfile.isValidUserName(username):
+                # the username/userslug does already exist
                 index = 2
                 while True:
-                    try:
-                        User.objects.get(username="%s %s"%(username,int_to_roman(index)))
-                    except User.DoesNotExist:
-                        #sweet, we found our username
-                        username="%s %s"%(username, int_to_roman(index))
+                    username="{} {}".format(username, int_to_roman(index))
+                    if UserProfile.isValidUserName(username):
                         break
-                    else:
-                        index += 1            
-            except User.DoesNotExist:
-                pass
+                    index += 1
             user.username = username
         else:
             user.email = ""
@@ -77,15 +68,21 @@ class ProfileCreationForm(UserCreationForm):
         return user
 
 class ProfileUpdateForm(forms.ModelForm):
-    username = forms.CharField(label=_("Username"))
 
     def __init__(self, *args, **kwargs):
         super(forms.ModelForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs['readonly'] = True # text input
+        # self.fields['username'].widget.attrs['readonly'] = True # text input
 
     class Meta:
         model = User
         fields = ("username", "first_name", "last_name", "email")
+
+    def clean_username(self):
+        # check if slug is unique (via UserProfile.isValidUserName)
+        username = self.cleaned_data["username"]
+        if not self.instance.profile.isValidUserNameChange(username):
+            raise forms.ValidationError(error_messages['duplicate_username'])
+        return username
 
     def clean_status(self):
         return self.instance.status
--- accounts/models.py
+++ accounts/models.py
@@ -1,18 +1,44 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
-from common.stringify import niceBigInteger
+from django.template.defaultfilters import slugify
 
 class UserProfile(models.Model):  
     user = models.OneToOneField(User, related_name="profile")
+    slug = models.SlugField(unique=True)
     karma = models.IntegerField(default=0)
     
+    @staticmethod
+    def isValidUserName(username):
+        """ check if slug derived from username already exists,
+            the username is then automatically also unique
+        """
+        userslug = slugify(username)
+        try:
+            UserProfile.objects.get(slug=userslug)
+            return False
+        except UserProfile.DoesNotExist:
+            return True
+
+    def isValidUserNameChange(self, username):
+        """ same as isValidUserName, but keeps into account already
+            existing model
+        """
+        userslug = slugify(username)
+        try:
+            profile = UserProfile.objects.get(slug=userslug)
+            return self.id == profile.id
+        except UserProfile.DoesNotExist:
+            return True
+
     @property
     def display_name(self):
         return self.user.username
     
 ### make sure profile is created when new user is created ###
 def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-       profile, created = UserProfile.objects.get_or_create(user=instance)  
+    profile, created = UserProfile.objects.get_or_create(user=instance)
+    if not created:
+        profile.slug = slugify(instance.username)
+        profile.save()
 post_save.connect(create_user_profile, sender=User)
