943ac6efd907780af3bbff68bc83b008e39982f5
Jens Nyman <nymanjens.nj@gmail.com>
2013-02-22 23:09:48 +0100

    bugfix: srv.py creates local settings automatically from template

--- srv.py
+++ srv.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
+import os, shutil
 
-import os
-import getopinionated.local_settings
+### make sure there are local settings ###
+try:
+    import getopinionated.local_settings
+except ImportError:
+    shutil.copy('getopinionated/local_settings.py.template', 'getopinionated/local_settings.py')
+    print "Note: Local settings created\n"
+    import getopinionated.local_settings
 
+### validate models ###
 status = os.system('python manage.py validate')
 
+### start server ###
 if(not status):
     os.system('python manage.py syncdb --noinput')
     os.system('python manage.py loaddata testdata.json')
