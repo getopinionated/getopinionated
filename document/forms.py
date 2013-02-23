f1a01145da827d6ba5fc4d1b3f243a66e4832949
Jonas Degrave <Jonas.Degrave@UGent.be>
2013-02-23 01:00:47 +0100

    Adding tags to proposals

--- document/forms.py
+++ document/forms.py
@@ -1,22 +1,30 @@
 from django import forms
 from models import Diff
 from document.models import FullDocument
-from proposing.models import Proposal 
+from proposing.models import Proposal, Tag
 
 class ProposalForm(forms.ModelForm):
     title = forms.CharField(widget=forms.TextInput())
     content = forms.CharField(widget=forms.Textarea(attrs={'cols': 80, 'rows': 20}))
     motivation = forms.CharField(widget=forms.Textarea(attrs={'cols': 80, 'rows': 20}))
+    
+
+    def get_my_choices(self):
+        return [(tag.name, tag.name) for tag in Tag.objects.all()]
 
     def __init__(self, *args, **kwargs):
         super(ProposalForm, self).__init__(*args, **kwargs)
+        self.fields['tags'] = forms.MultipleChoiceField(required=False, choices=self.get_my_choices(),widget=forms.SelectMultiple(attrs={'data-placeholder':"Choose tags", 'class':"chzn-select", 'multiple':'', 'tabindex':"4"}) )
         self.originalcontent = self.instance.content
 
     class Meta:
         model = FullDocument
         fields = ("content",)
 
-    def save(self, user, commit=True):
+    def clean_tags(self):
+        pass
+
+    def save(self, user, commit=True, tags=[]):
         newcontent = FullDocument.cleanText(self.cleaned_data["content"])
         newdiff = Diff.generateDiff(self.originalcontent,
                                     newcontent)
@@ -27,4 +35,6 @@ class ProposalForm(forms.ModelForm):
                                motivation = self.cleaned_data["motivation"],
                                diff = newdiff,
                                creator = user )
+        a = self.cleaned_data["tags"]
+        a.all()
         newproposal.save()
--- document/views.py
+++ document/views.py
@@ -5,6 +5,7 @@ from common.shortcuts import render_to_response
 from forms import ProposalForm
 from django.contrib.auth.decorators import login_required
 from models import FullDocument
+from proposing.models import Tag
 
 def documentView(request, document_slug, document_version=None):
     ## get the document
@@ -14,9 +15,10 @@ def documentView(request, document_slug, document_version=None):
         fulldocument = FullDocument.objects.get(slug=document_slug, version=document_version)
     ## Initialize the form either fresh or with the appropriate POST data as the instance
     if request.method == 'POST':
+        tags = request.POST.get('tags')
         form = ProposalForm(request.POST, instance=fulldocument)
         if form.is_valid():
-            form.save(user = request.user)
+            form.save(user = request.user, tags=tags)
             return HttpResponseRedirect(reverse('proposals-index'))
         else:
             pass
