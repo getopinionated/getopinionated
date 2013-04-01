3062516e096c4c30836076b9ca7d46bf3f05f438
Jens Nyman <nymanjens.nj@gmail.com>
2013-04-01 22:20:54 +0200

    added timeline

--- common/forms.py
+++ common/forms.py
@@ -5,8 +5,6 @@ class FocussingModelForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(FocussingModelForm, self).__init__(*args, **kwargs)
         # focus on first element at page-load (html5)
-        print self.fields
-        print self.fields.values()[0]
         self.fields.values()[0].widget.attrs.update({'autofocus' : 'autofocus'})
 
 class StarRequiredModelForm(forms.ModelForm):
