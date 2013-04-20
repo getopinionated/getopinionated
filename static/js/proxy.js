9ee75b18d81a507251f00e4bdc33286c32fab06b
Jonas Degrave <Jonas.Degrave@UGent.be>
2013-04-20 18:32:20 +0200

    Added proxy form with dynamic length

--- static/js/proxy.js
+++ static/js/proxy.js
@@ -2,6 +2,11 @@ var count = 0;
 $(document).ready(function(){
 	$('#addProxyButton').click(function(){
 		count += 1;
-		$('#proxy_list').append('<strong>Link #' + count + '</strong>'+ '<input id="field_' + count + '" name="fields[]' + '" type="text" />' );
+		$('#0.proxy_item').clone().attr('id',count).attr('style','').appendTo($('.proxy_list'));
+		$("#"+count+">select").each( function(){
+			currentid = $(this).attr('id');
+			$(this).attr('id',currentid+''+count);
+		});
+		$("#"+count+">select").chosen({no_results_text: "No results matched"});
 	});
 });
