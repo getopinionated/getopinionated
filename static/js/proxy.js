9384d172de0b935ec38c73349af50310e021605f
Jonas Degrave <Jonas.Degrave@UGent.be>
2013-04-22 02:10:25 +0200

    now renders proxies stored upon opening proxy form

--- static/js/proxy.js
+++ static/js/proxy.js
@@ -1,14 +1,19 @@
 var count = 0;
 $(document).ready(function(){
 	$('#addProxyButton').click(function(){
-		count += 1;
-		$('#0.proxy_item').clone().attr('id',count).attr('style','').appendTo($('.proxy_list'));
-		$("#"+count+">select").each( function(){
+		//as long the field already exists, keep counting
+		while( $("#item"+count).length > 0){
+			count += 1;
+		}
+		$('#itemhidden.proxy_item').clone().attr('id',"item"+count).attr('style','').appendTo($('.proxy_list'));
+		$("#item"+count+">select").each( function(){
 			currentid = $(this).attr('id');
 			$(this).attr('id',currentid+''+count);
 			currentname = $(this).attr('name');
 			$(this).attr('name',currentname+''+count);
 		});
-		$("#"+count+">select").chosen({no_results_text: "No results matched"});
+		$("#tagfieldcount").val(""+(count+1));
+		$("#item"+count+">select").chosen({no_results_text: "No results matched"});
+		count += 1;
 	});
 });
