f5ed2756268b62229e4653d3134632c7a63f7d10
Jonas Degrave <jonas@pirateparty.be>
2013-05-20 13:38:21 +0000

    Better testdata

--- create_test_data.sh
+++ create_test_data.sh
@@ -1,1 +1,1 @@
-python manage.py dumpdata -e contenttypes -e sessions -e admin -e auth.permission --indent=4 > testdata.json
+python2.7 manage.py dumpdata -e contenttypes -e sessions -e admin -e auth.permission --indent=4 > testdata.json
