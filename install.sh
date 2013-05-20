caaf9a5d00597bc01d67152be2dee20a2cecd8e6
317070 <erstaateenknolraapinmijntuin@gmail.com>
2013-05-20 15:18:54 +0200

    Updating install script

--- install.sh
+++ install.sh
@@ -2,26 +2,25 @@
 sudo apt-get install python-imaging
 
 # install memcached for caching
-sudo apt-get install memcached
+#sudo apt-get install memcached
 
 # install python binding for memcached: python-memcache (no 'd')
-sudo apt-get install python-memcache
+#sudo apt-get install python-memcache
 
 # install pip
 sudo apt-get install python-pip
 
-sudo apt-get install pip
-
 sudo pip install south
 
-sudo apt-get install python-dev
-sudo pip install fabric
+#sudo apt-get install python-dev
+#sudo pip install fabric
 
 #install social-auth
-sudo pip install easy_install
+#sudo pip install easy_install
 
 #install open-id
 sudo apt-get install python-openid
+sudo apt-get install python-oauth2
 
 #set up cron to update everything every 5 minutes
 
