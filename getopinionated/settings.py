2f8d7e72b862036dece4ecbebbb59da4e684cbad
Jonas Degrave <jonas@pirateparty.be>
2013-06-23 00:14:07 +0000

    Small fix in settings

--- getopinionated/settings.py
+++ getopinionated/settings.py
@@ -28,7 +28,7 @@ USE_I18N = True
 USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = False #TODO:fix
+USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
