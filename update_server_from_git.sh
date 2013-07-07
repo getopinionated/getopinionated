a81ad120e7395e4dea84a4566baae5076b3828aa
Jonas Degrave <jonas@pirateparty.be>
2013-07-07 22:43:28 +0000

    Fixed the database. South is a PITA! Somebody should figure out how to use it properly!

--- update_server_from_git.sh
+++ update_server_from_git.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #updating server and running maintenance scripts
-cd /home/de317070/webapps/getopinionated/getopinionated/getopinionated
-git fetch
-newUpdatesAvailable=`git diff HEAD FETCH_HEAD`
-if [ "$newUpdatesAvailable" != "" ]
-then
-	git update-index --assume-unchanged getopinionated/local_settings.py
-	git rm --cached getopinionated/local_settings.py
-	git reset --hard
-	git pull
-	python2.7 $HOME/webapps/getopinionated/getopinionated/manage.py collectstatic --noinput
-	$HOME/webapps/getopinionated/apache2/bin/restart
-fi
+#cd /home/de317070/webapps/getopinionated/getopinionated/getopinionated
+#git fetch
+#newUpdatesAvailable=`git diff HEAD FETCH_HEAD`
+#if [ "$newUpdatesAvailable" != "" ]
+#then
+#	git update-index --assume-unchanged getopinionated/local_settings.py
+#	git rm --cached getopinionated/local_settings.py
+#	git reset --hard
+#	git pull
+#	python2.7 $HOME/webapps/getopinionated/getopinionated/manage.py collectstatic --noinput
+#	$HOME/webapps/getopinionated/apache2/bin/restart
+#fi
