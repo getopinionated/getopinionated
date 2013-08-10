30315729f00b2a431a9b30a675dc78f8b3e9ebac
Jens Nyman <nymanjens.nj@gmail.com>
2013-08-10 22:28:27 +0200

    made the endorsements slightly more easy to understand for an outsider and added a settings_value templatetage

--- getopinionated/settings.py
+++ getopinionated/settings.py
@@ -306,6 +306,7 @@ SOCIAL_AUTH_FORCE_POST_DISCONNECT = True
 #####################################################################################
 ANONYMOUS_PROPOSALS = True # allow anonymous proposals if True
 ANONYMOUS_COMMENTS = True # allow anonymous comments if True
+MINIMAL_UPVOTES_BEFORE_VOTING = 3
 
 #####################################################################################
 # Import local settings
