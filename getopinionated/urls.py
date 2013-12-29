4abc1108d3a9b2ab535f1bd19f1de48a47b8585d
Jens Nyman <nymanjens.nj@gmail.com>
2013-12-29 22:21:52 +0100

    added under maintenance page (setting)

--- getopinionated/urls.py
+++ getopinionated/urls.py
@@ -11,16 +11,21 @@ from getopinionated.settings import MEDIA_ROOT
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-	(r'^admin/', include(admin.site.urls)),
-	# (r'^static/(?P<path>.*)/$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    url(r'^$', views.home, name="home-index"),
-    (r'^accounts/', include('accounts.urls')),
-	(r'^proposals/', include('proposing.urls')),
-	(r'^document/', include('document.urls')),
-	url(r'^tag/(?P<tag_slug>[-\w]+)/$', proposing.views.tagproplist, name='tag-index'),
-	(r'^media/(?P<path>.*)/$', 'django.views.static.serve',{'document_root': MEDIA_ROOT, }),
-	url(r'', include('social_auth.urls')),
-	url(r'^about/', views.about, name='about'),
-)
+if settings.UNDER_MAINTENANCE:
+	urlpatterns = patterns('',
+		url(r'^', views.under_maintenance, name="under_maintenance"),
+	)
+else:
+	urlpatterns = patterns('',
+		(r'^admin/', include(admin.site.urls)),
+		# (r'^static/(?P<path>.*)/$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+	    url(r'^$', views.home, name="home-index"),
+	    (r'^accounts/', include('accounts.urls')),
+		(r'^proposals/', include('proposing.urls')),
+		(r'^document/', include('document.urls')),
+		url(r'^tag/(?P<tag_slug>[-\w]+)/$', proposing.views.tagproplist, name='tag-index'),
+		(r'^media/(?P<path>.*)/$', 'django.views.static.serve',{'document_root': MEDIA_ROOT, }),
+		url(r'', include('social_auth.urls')),
+		url(r'^about/', views.about, name='about'),
+	)
 
