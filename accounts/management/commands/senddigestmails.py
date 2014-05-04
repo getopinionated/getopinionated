b90684c4cbef082674a6b94dd9a4bee7192f14ed
Jens Nyman <nymanjens.nj@gmail.com>
2014-05-04 15:07:08 +0200

    implemented mailer

--- accounts/management/commands/senddigestmails.py
+++ accounts/management/commands/senddigestmails.py
@@ -1,6 +1,7 @@
 import random
 from collections import OrderedDict
 from smtplib import SMTPRecipientsRefused
+
 from django.core.management.base import BaseCommand, CommandError, NoArgsCommand, LabelCommand
 from django.db.models import Q
 from django.contrib.auth.models import User
@@ -9,9 +10,11 @@ from django.utils import timezone
 from django.utils.timezone import datetime, timedelta, now
 from django.conf import settings
 from django.core.mail import send_mail
+
 from accounts.models import CustomUser, UnsubscribeCode
 from proposing.models import Proposal, ProxyProposalVote, Proxy, Tag
 from event.models import PersonalEventEmailQueue, GlobalEventEmailQueue, ProposalLifeCycleEvent
+from event.templatetags.event_filters import BundledEvent, listeners_to_bundled_events
 
 class Command(LabelCommand):
     help = '''Send a mail to the users regarding new events.
@@ -42,17 +45,18 @@ class Command(LabelCommand):
                 continue
 
             ### get all relevant events from queue ###
-            events = []
-
             # pop personal events
+            personal_event_listeners = []
             if user.mail_when_related_event:
                 for item in PersonalEventEmailQueue.objects.filter(event_listener__user=user):
                     event_listener = item.event_listener
                     if not event_listener.seen_by_user:
-                        events.append(event_listener.event.cast())
+                        event_listener.event = event_listener.event.cast()
+                        personal_event_listeners.append(event_listener)
                     item.delete()
             
             # get global events
+            global_events = []
             for item in GlobalEventEmailQueue.objects.all():
                 if user not in item.already_mailed_users.all():
                     event = item.event.cast()
@@ -72,18 +76,28 @@ class Command(LabelCommand):
                         raise NotImplementedError("Global events of type {} are not supported by the mailer yet.".format(type(event)))
 
                     if eligible_event:
-                        events.append(event)
+                        global_events.append(event)
                         item.already_mailed_users.add(user)
 
-            events = list(OrderedDict.fromkeys(events)) # filter duplicate events while keeping order
+            # filter duplicate events between personal events and global events
+            personal_event_listeners = [p for p in personal_event_listeners if p.event in global_events]
 
+            ### convert events to bundledevents ###
+            bundledevents = []
+            # add personal events
+            bundledevents += listeners_to_bundled_events(personal_event_listeners)
+
+            # add global events
+            bundledevents += [BundledEvent(events=[event], unseen_events=[], reading_user=user) for event in global_events]
+
+            ### get email text ###
             unsubscribecode = UnsubscribeCode(user=user, code=random.SystemRandom().getrandbits(64))
             unsubscribecode.save()
             mail_text = render_to_string('mails/digestmail.html', dictionary={
 				'DOMAIN_NAME': settings.DOMAIN_NAME,
                 'unsubscribecode': unsubscribecode,
                 'mail_frequency': self._HUMAN_READABLE_MAIL_FREQUENCY[mail_frequency],
-                'events': events,
+                'bundledevents': bundledevents,
                 'user': user,
             })
 
